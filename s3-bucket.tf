59ad780e977bdf2e27053c5200a36711820ff8b5
Paul Fordahl <pfordahl@users.noreply.github.com>
2021-03-23 13:54:18 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "pfordal78-training-my-s3-bucket"
   acl    = "private"
 
   versioning = {
